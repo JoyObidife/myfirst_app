583fbfd7323b28ab9cf7ef3af635f00f4b5f6cb2
Joy obidife <obidifejoy@gmail.com>
2025-10-08 22:01:31 +0100

    Initial_code

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:myfirst_app/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Save A Life',
+      theme: ThemeData(
+        
+        
+        
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+      
+      ),
+ debugShowCheckedModeBanner: false,
+      home: HomePage()
+    );
+  }
+}
+
