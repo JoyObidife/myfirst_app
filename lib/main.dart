d5ad722abaf18bbbb8a18a846d508bfe2d70774b
Joy obidife <obidifejoy@gmail.com>
2025-10-17 23:25:05 +0100

    continuation on onboarding_page

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
   "/":(context)=> OnbordingPage(),
   "/home": (context)=> BottomNavigation(), 
  },
-      home: BottomNavigation()
+     // home: BottomNavigation()
     );
   }
 }
--- lib/onbording_page.dart
+++ lib/onbording_page.dart
@@ -8,14 +8,84 @@ class OnbordingPage extends StatefulWidget {
 }
 
 class _OnbordingPageState extends State<OnbordingPage> {
+  // for tracking what is next and previous, variable or state was created.
+  int activeIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        spacing: 16,
         children: [
-          Image.asset("assets/onboardingscreen.png"),
+          CustomIndicator(
+            indicatorCount: 3, activeIndex: activeIndex,
+            ),
+
+          Image.asset("assets/onboardingscreenn.jpg", width: 350, height: 350),
+          Text(
+            "Welcome to MediCall",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+          ),
+          Text(
+            "We help you make ambulance requests for emergency and other purposes",
+            style: TextStyle(fontSize: 18),
+            textAlign: TextAlign.center,
+          ),
         ],
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            activeIndex<=0 ? SizedBox(height: 0, width: 0,) :  ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  activeIndex = activeIndex - 1;
+                }); 
+              },
+              child: Text("Back"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  activeIndex = activeIndex + 1;
+                });
+              },
+              child: Text("Next"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomIndicator extends StatelessWidget {
+  const CustomIndicator({
+    super.key, 
+    required this.indicatorCount,
+    required this.activeIndex, 
+    });
+  final int indicatorCount;
+  // how to make the top buttons to look faded.
+  final int activeIndex;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: 2,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: List.generate(
+        indicatorCount,
+
+        (index) => Container(
+          decoration: BoxDecoration(shape: BoxShape.circle, color: activeIndex == index ? Colors.blue : Colors.blue.shade200,),
+          height: 10,
+          width: 10,
+        ),
       ),
     );
   }
