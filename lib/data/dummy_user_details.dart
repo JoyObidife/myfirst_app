bb3b3239577dd19022381f68c47a371add625315
Joy obidife <obidifejoy@gmail.com>
2025-10-30 03:47:40 +0100

    correction made on dummy_user_detail

--- lib/data/dummy_user_details.dart
+++ lib/data/dummy_user_details.dart
@@ -4,5 +4,5 @@ const USER_DETAILS = [
   UserDetail(name: "Chioma", email: "chioma@gmail.com", profilePicture: ""),
   UserDetail(name: "Obinna", profilePicture: "", email: "obinna@gmail.com"),
   UserDetail(name: "Nonye", profilePicture: "", email: "nonye@gmail.com"),
-  UserDetail(name: "Chidi", profilePicture: "", email: "chidi@gmail.com"),
+  
 ];
