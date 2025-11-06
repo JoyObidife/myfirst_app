d92453f052c3bd5a4e30d988ccd61f7e47e2f4b8
Joy obidife <obidifejoy@gmail.com>
2025-11-06 03:37:57 +0100

    implementation of snackbar

--- lib/data/dummy_user_details.dart
+++ lib/data/dummy_user_details.dart
@@ -1,8 +1,8 @@
 import 'package:myfirst_app/model/user_detail.dart';
 
 var USER_DETAILS = [
-  UserDetail(name: "Chioma", email: "chioma@gmail.com", profilePicture: ""),
-  UserDetail(name: "Obinna", profilePicture: "", email: "obinna@gmail.com"),
-  UserDetail(name: "Nonye", profilePicture: "", email: "nonye@gmail.com"),
+  UserDetail(name: "Chioma", email: "chioma@gmail.com", profilePicture: "", password: "4775"),
+  UserDetail(name: "Obinna", profilePicture: "", email: "obinna@gmail.com", password: "0000"),
+  UserDetail(name: "Nonye", profilePicture: "", email: "nonye@gmail.com", password: "2222"),
   
 ];
