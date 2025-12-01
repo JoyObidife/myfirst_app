14cb3d5796b7523689f5b9d1e4fe0b4000830325
JoyObidife <obidifejoy@gmail.com>
2025-11-30 18:03:19 -0800

    used json_serializable on user details

--- lib/model/user_detail.dart
+++ lib/model/user_detail.dart
@@ -1,31 +1,39 @@
 
+import 'package:json_annotation/json_annotation.dart';
+part 'user_detail.g.dart';
+ 
+@JsonSerializable()
 class UserDetail {
-   const UserDetail({
-    
-   required this.name,
-     this.profilePicture = "",
+  const UserDetail({
+    required this.name,
+    this.profilePicture = "",
     required this.email,
-   this.phoneNumber = "",
+    this.phoneNumber = "",
     this.address = "",
     this.occupation = "",
   });
-  
+ 
   final String name;
   final String profilePicture;
   final String email;
   final String phoneNumber;
   final String address;
   final String occupation;
-
-  // this map was used to convert userdetail to map for firestore can access it
-  Map<String, dynamic> toJson() {
-    return {
-      "name": name,
-      "email": email,
-      "phoneNumber": phoneNumber,
-      "address": address,
-      "profilePicture": profilePicture,
-      "occupation": occupation,
-    };
-  }
+ 
+  factory UserDetail.fromJson(Map<String, dynamic> json) => _$UserDetailFromJson(json);
+ 
+  Map<String, dynamic> toJson() => _$UserDetailToJson(this);
 }
+ 
+  // this map was used to convert userdetail to map for firestore can access it
+  // Map<String, dynamic> toJson() {
+  //   return {
+  //     "name": name,
+  //     "email": email,
+  //     "phoneNumber": phoneNumber,
+  //     "address": address,
+  //     "profilePicture": profilePicture,
+  //     "occupation": occupation,
+  //   };
+  
+//}
